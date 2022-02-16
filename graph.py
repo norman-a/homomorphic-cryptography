01cf77a7d5c4fd4f10c1e4c2d9e126a9ab8aa63a
Kenneth West <45572572+KennethWest@users.noreply.github.com>
2022-02-16 13:17:14 -0500

    Add files via upload

--- graph.py
+++ graph.py
@@ -41,8 +41,8 @@ class Graph(object):
             print("vertex ", v2, " doesn't exist!")
         elif v1 == v2:
             print("two vertices can't be equal!")
-        elif [v2, w] in self.adj_list[v1]:
-            print(v1, ", ", v2, ", ", w, "edge already exists!")
+        #elif [v2, w] in self.adj_list[v1]:
+        #    print(v1, ", ", v2, ", ", w, "edge already exists!")
         else:
             if not self._directed:
                 temp2 = [v1, w]
@@ -58,42 +58,50 @@ class Graph(object):
             self.adj_list.pop(v)
 
     def remove_edge(self, v1, v2, w=None):
-        if v1 in self.adj_list and v2 in self.adj_list \
-           and [v2, w] in self.adj_list[v1] and [v1, w] in self.adj_list[v2]:
+        if v1 in self.adj_list and v2 in self.adj_list:
             self.adj_list[v1].remove([v2, w])
-            if not self._directed:
+            if [v1, w] in self.adj_list[v2]:
                 self.adj_list[v2].remove([v1, w])
             return True
         else:
-            print("Edge doesn't exist!")
             return False
 
     def encrypt_g2(self):
         """
-        Encryption of the graph by adding 20 random vertices and 50 random edges
+        Encryption of the graph by adding sufficient vertices n and sufficient edges m
         to the graph
         """
-        # add 20 vertices to the graph
-        for _ in range(20):
-            x = random.randint(0, 50)
+        alpha = random.randint(1, 10)
+        beta = random.randint(1, 10)
+        gamma = random.randint(1, 10)
+        delta = random.randint(1, 10)
+        n_1 = len(self.adj_list.keys())
+        n = alpha * n_1 + beta
+        n_2 = n + n_1
+        m = int((gamma * n_2) * ((n_2 - 1)/2) + delta)
+
+        # add vertices to the graph
+        for i in range(1, n + 1):
+            x = n_1 + i
             self.add_vertex(x)
 
-        # add 50 edges to the graph
-        for _ in range(50):
+        self._key.append(n_1)
+        # add edges to the graph
+        k = 0
+        while k < m:
             x = random.choice(list(self.adj_list.keys()))
             y = random.choice(list(self.adj_list.keys()))
             if x == y:
+                k -= 1
                 continue
             w = random.randint(1, 10)
             self.add_edge(x, y, w)
-            if self.in_key(x, y):
-                self._key.append([x, y, w, self.in_key(x, y)[3]])
-            else:
-                o = random.randint(0, 1)
-                self._key.append([x, y, w, o])
-
+            o = random.randint(0, 1)
+            self._key.append([x, y, w, o])
+            k += 1
         # sort the key to be able to merge edges in encrypt_g3()
-        self._key.sort()
+        # self._key.sort()
+        print(self._key)
 
     def encrypt_g3(self):
         """
@@ -102,27 +110,88 @@ class Graph(object):
         weights.
         """
         # get the first item of the ._key and set it to current
-        curr_v1, curr_v2, curr_w = self._key[0][0], self._key[0][1], self._key[0][2]
+        curr_v1, curr_v2, curr_w = self._key[1][0], self._key[1][1], self._key[1][2]
 
         # starting from the second item, iterate through the edges
-        for edge in self._key[1:]:
+        for edge in self._key[2:]:
             # remove every edge and calculate the weight associated between the
             # current pair
             self.remove_edge(edge[0], edge[1], edge[2])
             if edge[0] == curr_v1 and edge[1] == curr_v2:
-
                 if edge[3] == 0:
                     curr_w += edge[2]
                 else:
                     curr_w *= edge[2]
 
             else:
-                # add the final egdge and set current to the next pair
+                # add the final edge and set current to the next pair
                 self.add_edge(curr_v1, curr_v2, curr_w)
                 curr_v1 = edge[0]
                 curr_v2 = edge[1]
                 curr_w = edge[2]
         self.add_edge(curr_v1, curr_v2, curr_w)
+        random_vert_list = []
+        key_mapping = []
+        for _ in range(self.vertex_count * 2):
+            r = random.randint(1, 1000)
+            if r not in random_vert_list:
+                random_vert_list.append(r)
+        new_adj_list = {}
+        for v in self.adj_list:
+            i = random.randint(0, len(random_vert_list) - 1)
+            new_v = random_vert_list[i]
+            random_vert_list.remove(new_v)
+            new_adj_list[new_v] = self.adj_list[v]
+            # vertex mappings are a tuple that represents hidden vertex -> original vertex as (hidden, original)
+            key_mapping.append((new_v, v))
+        for v in new_adj_list:
+            edges = new_adj_list[v]
+            for i in range(len(edges) - 1):
+                for pair in key_mapping:
+                    if edges[i][0] == pair[1]:
+                        edges[i][0] = pair[0]
+            new_adj_list[v] = edges
+        self._key.append(key_mapping)
+        self.adj_list = new_adj_list
+
+    def decrypt(self):
+        # first we restore vertices to their original identities
+        new_adj_list = {}
+        for v in self.adj_list:
+            for pair in self._key[-1]:
+                if v == pair[0]:
+                    new_adj_list[pair[1]] = self.adj_list[v]
+        for v in new_adj_list:
+            edges = new_adj_list[v]
+            for i in range(len(edges) - 1):
+                for pair in self._key[-1]:
+                    if edges[i][0] == pair[0]:
+                        edges[i][0] = pair[1]
+            new_adj_list[v] = edges
+        self.adj_list = new_adj_list
+        iterate = self.adj_list.copy()
+        for v in iterate:
+            if v > self._key[0]:
+                self.remove_vertex(v)
+        print(self.adj_list)
+        for v in self.adj_list:
+            i = 0
+            for edge in iterate[v]:
+                if edge[0] > self._key[0]:
+                    del self.adj_list[v][i]
+                i += 1
+        print(self.adj_list)
+        for key in self._key[1:-1]:
+            for v in self.adj_list:
+                if key[0] == v or key[1] == v:
+                    i = 0
+                    for edge in self.adj_list[v]:
+                        if edge[0] == key[0] or edge[0] == key[1]:
+                            if key[3] == 0:
+                                self.adj_list[v][i] = [self.adj_list[v][i][0], self.adj_list[v][i][1] - key[2]]
+                            else:
+                                self.adj_list[v][i] = [self.adj_list[v][i][0], int(self.adj_list[v][i][1]/key[2])]
+                        i += 1
 
     def __str__(self):
         """
@@ -132,6 +201,17 @@ class Graph(object):
         for vertex in self.adj_list:
             for edge in self.adj_list[vertex]:
                 final += "{} -> {}, edge weight: {} \n".format(vertex,
-                                                          edge[0], edge[1])
+                                                               edge[0], edge[1])
         return final
 
+"""
+    def remove_edge(self, v1, v2, w=None):
+        if v1 in self.adj_list and v2 in self.adj_list \
+                and [v2, w] in self.adj_list[v1] and [v1, w] in self.adj_list[v2]:
+            self.adj_list[v1].remove([v2, w])
+            if not self._directed:
+                self.adj_list[v2].remove([v1, w])
+            return True
+        else:
+            return False
+"""
--- main.py
+++ main.py
@@ -7,33 +7,33 @@ from graph import Graph
 if __name__ == '__main__':
     g = Graph()
 
-    # g.add_vertex(1)
-    # g.add_vertex(2)
-    # g.add_vertex(3)
-    # g.add_vertex(4)
-    #
-    # g.add_edge(1, 3, 4)
-    # g.add_edge(2, 3, 5)
-    # g.add_edge(4, 3, 2)
-    # print(g)
+    g.add_vertex(1)
+    g.add_vertex(2)
+    g.add_vertex(3)
+    g.add_vertex(4)
+    g.add_vertex(5)
+
+    g.add_edge(1, 2, 4)
+    g.add_edge(1, 3, 7)
+    g.add_edge(1, 4, 9)
+    g.add_edge(1, 5, 4)
+    g.add_edge(2, 3, 5)
+    g.add_edge(2, 4, 6)
+    g.add_edge(2, 5, 8)
+    g.add_edge(3, 4, 10)
+    g.add_edge(3, 5, 2)
+    g.add_edge(4, 5, 1)
+    print(g)
 
     g.encrypt_g2()
     print(g)
-    print(g.return_key())
 
     g.encrypt_g3()
     print(g)
+
+    g.decrypt()
+    print(g)
+    print(g.adj_list)
 # Add the edges between the vertices by specifying
 # the from and to vertex along with the edge weights.
 
-
-    # g.encrypt_g2()
-    #
-    # print(g.return_key())
-    #
-    # print(g)
-    #
-    # g.encrypt_g3()
-    # print("--- G3 ----")
-    # print(g)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
